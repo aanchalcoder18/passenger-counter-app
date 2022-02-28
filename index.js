adb65ae7119a692e02f29b3434e15bb9d9c911a3
aanchalcoder18 <aasaxena14@gmail.com>
2022-02-28 11:19:35 -0800

    completed challenge

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let countEl = document.getElementById("count-el");
 let count = 0;
+let saveEl = document.getElementById("save-el");
 
 function increment(){
     count++;
     countEl.innerHTML = count;
+}
+
+function save(){
+    let countStr = count + " , ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
